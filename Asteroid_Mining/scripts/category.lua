c757aa5e129247d4a9cb2b652aa2fa4dededec6e
Silari <Silari@users.noreply.github.com>
2025-05-15 11:31:04 -0400

    Asteroid Mining version 2.0.11
    Version: 2.0.11  
      Bugfixes:
        - Fixed asteroid/chunk crushing recipes not using Krastorio 2 crushers even when option was enabled due to rename of the recipe category. This was causing Krastorio 2 to make crushing recipes for them which returned no materials.

--- Asteroid_Mining/scripts/category.lua
+++ Asteroid_Mining/scripts/category.lua
@@ -1,8 +1,8 @@
 reccategory = "crafting" -- Recipe category for asteroid processing to use
 
 -- If Krastorio2 is installed and crafting category for crushing exists, we'll use that for processing chunks
-if settings.startup["astmine-k2crushing"].value and data.raw["recipe-category"]["crushing"] then
-    reccategory = "crushing"
+if settings.startup["astmine-k2crushing"].value and data.raw["recipe-category"]["kr-crushing"] then
+    reccategory = "kr-crushing"
 end
 
 --If Angel's Crushing category exists and setting isn't off, use it for chunk crushing. This overrides K2.
